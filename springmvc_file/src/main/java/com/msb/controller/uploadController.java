90c4fa99c725d87bfa9c5e397bb448fe944f77e1
chenjie <jiechen220606@163.com>
2023-08-13 13:14:09 +0800

    把文件放在当前项目下，作为静态资源并解决文件名冲突问题

--- springmvc_file/src/main/java/com/msb/controller/uploadController.java
+++ springmvc_file/src/main/java/com/msb/controller/uploadController.java
@@ -5,21 +5,34 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.http.HttpServletRequest;
 import java.io.File;
 import java.io.IOException;
+import java.util.UUID;
 
 @Controller
 public class uploadController {
 
     @ResponseBody
     @RequestMapping("fileUpload.do")
-    public String uploadFile(MultipartFile headPhoto) throws IOException {
-        //指定文件存储目录
-        File file = new File("/Users/jiechen/Documents/study/dataStudy/javaStudy/SpringMVC/springmvc_file/photo");
+    public String uploadFile(MultipartFile headPhoto, HttpServletRequest request) throws IOException {
+        //获取磁盘中upload路径
+        String upload = request.getServletContext().getRealPath("upload");
+        //创建文件对象
+        File file = new File(upload);
+        //判断目录是否存在，若不存在直接创建
+        if(file.exists())
+        {
+            //直接创建多层目录
+            file.mkdirs();
+        }
         //获取上传的文件名
         String originalFilename = headPhoto.getOriginalFilename();
+        //获取拓展名
+        String extendName = originalFilename.substring(originalFilename.lastIndexOf("."));
+        String newFileName = UUID.randomUUID() + extendName;
         //生成文件存储位置
-        File storageFile = new File(file, originalFilename);
+        File storageFile = new File(file, newFileName);
         //文件保存
         headPhoto.transferTo(storageFile);
         return "ok";
